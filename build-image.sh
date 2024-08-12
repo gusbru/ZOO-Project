dc7514a9cb929aec6c5db6f78362fb52fb517975
gustavo.brunetto@earthdaily.com <you@example.com>
2024-08-12 17:31:58 +0000

    add build-image script

---
+++ build-image.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+docker build \
+  -t public.ecr.aws/k4l9p8h0/eoepca/zoo-project:0.0.45 \
+  --build-arg CONDA_ENV_NAME=env_zoo_calrissian \
+  --build-arg PY_VER=3.10 \
+  --build-arg CONDA_ENV_FILE=https://raw.githubusercontent.com/gusbru/eoepca-proc-service-template/testArgoAdesV2/.devcontainer/environment.yml \
+  --no-cache \
+  -f docker/dru/Dockerfile  .
+
+docker push public.ecr.aws/k4l9p8h0/eoepca/zoo-project:0.0.45
