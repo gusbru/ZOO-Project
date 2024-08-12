5694f8234942f489abd9dafa123fd208479944c6
Gustavo Brunetto <gustavo.brunetto@earthdaily.com>
2024-08-12 12:54:44 -0700

    update version

--- build-image.sh
+++ build-image.sh
@@ -2,11 +2,11 @@
 set -e
 
 docker build \
-  -t public.ecr.aws/k4l9p8h0/eoepca/zoo-project:0.0.45 \
+  -t public.ecr.aws/k4l9p8h0/eoepca/zoo-project:0.0.46 \
   --build-arg CONDA_ENV_NAME=env_zoo_calrissian \
   --build-arg PY_VER=3.10 \
   --build-arg CONDA_ENV_FILE=https://raw.githubusercontent.com/gusbru/eoepca-proc-service-template/testArgoAdesV2/.devcontainer/environment.yml \
   --no-cache \
   -f docker/dru/Dockerfile  .
 
-docker push public.ecr.aws/k4l9p8h0/eoepca/zoo-project:0.0.45
+docker push public.ecr.aws/k4l9p8h0/eoepca/zoo-project:0.0.46
